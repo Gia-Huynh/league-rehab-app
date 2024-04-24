995a8588380e3ca33879231e6d666d76a7ce4ff4
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2024-04-25 01:14:58 +0700

    minor adjustment

--- HelpScript/StartBuild.bat
+++ HelpScript/StartBuild.bat
@@ -1,5 +1,5 @@
 cd C:\Users\Za\Desktop\SchoolWork\Nam 4\LinhTinhT\league-rehab-app\HelpScript
 cd ..
 pyinstaller --onefile sex.py
-pyinstaller --onefile generateConfig.py
+pyinstaller --noconsole --onefile generateConfig.py
 pause
